72ed01186fdb07bc183aefa74528251ea04bbf39
Alex Tarnawski <amtarnawski@gmail.com>
2021-07-13 12:29:26 -0700

    removed test that used react-test-renderer

--- client/src/Components/CreateRoom/__test__/CreateLink.test.jsx
+++ client/src/Components/CreateRoom/__test__/CreateLink.test.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import renderer, { act } from 'react-test-renderer';
-import CreateLink from '../CreateLink';
+// import React from 'react';
+// import renderer, { act } from 'react-test-renderer';
+// import CreateLink from '../CreateLink';
 
-test('Link changes the class when hovered', () => {
-  let component;
-  act(() => {
-    component = renderer.create(
-      <CreateLink href="/home/alex/hr/harmony/client/dist/room.html">Friend</CreateLink>,
-    );
-  });
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+// test('Link changes the class when hovered', () => {
+//   let component;
+//   act(() => {
+//     component = renderer.create(
+//       <CreateLink href="/home/alex/hr/harmony/client/dist/room.html">Friend</CreateLink>,
+//     );
+//   });
+//   let tree = component.toJSON();
+//   expect(tree).toMatchSnapshot();
 
-  // manually trigger the callback
-  act(() => {
-    tree.props.onMouseEnter();
-  });
+//   // manually trigger the callback
+//   act(() => {
+//     tree.props.onMouseEnter();
+//   });
 
-  // re-render
-  tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+//   // re-render
+//   tree = component.toJSON();
+//   expect(tree).toMatchSnapshot();
 
-  // manually trigger the callback
-  act(() => {
-    tree.props.onMouseLeave();
-  });
-  // re-rendering
-  tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
+//   // manually trigger the callback
+//   act(() => {
+//     tree.props.onMouseLeave();
+//   });
+//   // re-rendering
+//   tree = component.toJSON();
+//   expect(tree).toMatchSnapshot();
+// });
