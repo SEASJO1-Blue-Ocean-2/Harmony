1522661f3d45b991a0f6d4d06d787662d04c8a36
Yichun Jin <jinyichun17@gmail.com>
2021-07-13 16:46:27 -0700

    refacorty server.js

---
+++ client/src/Components/room/videoChannel.jsx
@@ -0,0 +1,15 @@
+
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/analytics';
+import 'firebase/database';
+import { addData } from '../util.js';
+import { useList } from 'react-firebase-hooks/database';
+
+const VideoChannel = () => {
+
+  return (<div id="video-grid"></div>);
+};
+
+
+export default VideoChannel;
--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/./client/dist');
 app.use("/", express.static('./client/dist/'));
 
-app.listen(PORT, function() {
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(express.json());
+
+app.get('/', function (req, res) {
+  // NEW CODE
+  res.render('index');
+ })
+
+
+
+server.listen(PORT, function() {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
