2c41e6f3c466eabc0ecf56e633d9fd24f0e93682
Alex Tarnawski <amtarnawski@gmail.com>
2021-07-15 15:02:09 -0700

    made create channel div with form and button

--- client/src/Components/Rooms/CreateChannel.jsx
+++ client/src/Components/Rooms/CreateChannel.jsx
@@ -1,9 +1,60 @@
 import React, { useState } from "react";
 
-function CreateChannel() {
+const { v4: uuidV4 } = require("uuid");
+
+function CreateChannel({ forTest }) {
+  const [create, setCreate] = useState(false);
+  const [newChanName, setNewChanName] = useState('');
+
+  if (forTest) {
+    setCreate(true);
+  }
+
+  function showCreate(isShown) {
+    if (isShown) {
+      setCreate(false);
+    } else {
+      setCreate(true);
+    }
+  }
+
+  function updateNewChanName() {
+
+  }
+
+  function createHandler() {
+
+  }
+
   return(
     <div data-testid="create-channel">
-      <button type="button">Create Channel</button>
+      <button
+        type="button"
+        onClick={() => showCreate()}
+      >
+        Create Channel
+      </button>
+      {create ?
+        <div className="row-default">
+          <span>
+            Name:
+          </span>
+          <span>
+            <input
+              type="text"
+              onChange={updateNewChanName}
+            />
+          </span>
+          <span>
+            <button
+              type="button"
+              onClick={createHandler}
+            >
+              Create
+            </button>
+          </span>
+        </div>
+      : null}
     </div>
   );
 }
--- client/src/Components/Rooms/__test__/CreateChannel.test.jsx
+++ client/src/Components/Rooms/__test__/CreateChannel.test.jsx
@@ -12,18 +12,29 @@
      render(
        <CreateChannel
          // pass all the props necessary for a basic render
-
+         forTest={false}
        />,
      );
      const element = await screen.getByTestId('create-channel');
      expect(element).toBeInTheDocument();
    });
-   test('should have console errors', async () => {
+
+   test('shouldn\'t crash if create menu open', async () => {
+    render(
+      <CreateChannel
+        // pass all the props necessary for a basic render
+        forTest={true}
+      />,
+    );
+    const element = await screen.getByTestId('create-channel');
+    expect(element).toBeInTheDocument();
+  });
+   test('should not have console errors', async () => {
      const consoleErrorSpy = jest.spyOn(console, 'error');
      render(
        <CreateChannel
          // pass all the props necessary for a basic render
-
+         forTest={false}
        />,
      );
      expect(consoleErrorSpy).not.toHaveBeenCalled();
