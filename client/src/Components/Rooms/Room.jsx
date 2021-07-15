59cb6943e065c7528a6f763d5806383674bd40bd
Simon <simonglu2@gmail.com>
2021-07-15 15:09:24 -0700

    implemented scroll bar

--- client/src/Components/Rooms/Room.jsx
+++ client/src/Components/Rooms/Room.jsx
@@ -83,7 +83,7 @@ const Room = ({ db, auth, roomId }) => {
 const MessageView = ({ channelId, db, uid }) => {
   const [messages, load, err] = useList(db.ref('/messages/' + channelId));
   return (
-    <div>
+    <div className='messageContainer'>
       {!load && messages.map(message => {
         return <Message key={message.key} data={message.val()} uid={uid} />
       })}
