d25c9d75230f409c303353654e6177e0966f82fe
Simon Lu <simonglu2@gmail.com>
2021-07-15 16:21:14 -0700

    Revert "Signup to create userData datapoint in database implemented"

--- client/src/Components/Rooms/RoomsRoutes.jsx
+++ client/src/Components/Rooms/RoomsRoutes.jsx
@@ -11,27 +11,25 @@ import CreateRoom from '../CreateRoom/CreateRoom.jsx';
 import Room from './Room.jsx';
 
 
-const RoomsRoutes = ({ auth, db }) => {
-  return (
-    <Router>
+const RoomsRoutes = ({ auth, db, user, friendsList}) => {
+return (
+  <Router>
       <Switch>
         <Route
           path='/rooms'
-          render={() => <RoomsList auth={auth} db={db} />}
+          render={() => <RoomsList auth={auth} db={db}/>}
         />
 
-        <Route path='/room/:roomid'
-        render={match =>  <Room auth={auth} db={db} roomId={match.match.params.roomid}/>}
-        />
+        <Route path='/room/:roomid' render={match =>  <Room auth={auth} db={db} roomId={match.match.params.roomid}/>}/>
 
         <Route
           path="/create"
           render={() => <CreateRoom user={user} db={db} friendsList={friendsList} />}
         />
 
+
       </Switch>
     </Router>
-  )
-}
+)}
 
 export default RoomsRoutes;
