b8058d6c762b99e5c5bbb5c62b79058b2d30c764
Julian Thong <julianzthong@gmail.com>
2021-07-16 09:40:40 -0700

    prepull commit

--- client/src/Components/login/LoginView.jsx
+++ client/src/Components/login/LoginView.jsx
@@ -52,7 +52,9 @@ const Login = ({ user, auth }) => {
             email: email
           }
         }} />}
+
       {user && <button onClick={signOut}>Sign Out</button>}
+
       <div className="login-logo">
         <img src='https://image.flaticon.com/icons/png/512/1820/1820090.png' id={css.harmonyLogo}>
         </img></div>
