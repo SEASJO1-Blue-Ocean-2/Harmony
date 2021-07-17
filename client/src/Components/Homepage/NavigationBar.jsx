8b3e1bacc3767b139cb3679f269aa28285f6a6a8
Alex Ting <alexting6366@gmail.com>
2021-07-17 11:33:52 -0700

    Code cleanup for Homepage

--- client/src/Components/Homepage/NavigationBar.jsx
+++ client/src/Components/Homepage/NavigationBar.jsx
@@ -1,37 +1,16 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import SearchBar from './SearchBar.jsx';
-import MainPage from './MainPage.jsx';
-import Logout from './Logout.jsx'
-class NavigationBar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-  render() {
-    return (
-      <>
-        <div>
-          <Link to='/' exact='true'>
-          {this.props.user && <input type='image' src='https://img.icons8.com/material/452/home--v5.png' className='homePageIcon'></input> }
-          </Link>
-
-          <h1 className='Harmony'>harmony</h1>
-          <Link to='/profile'>
-          {this.props.user &&  <img src='http://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Image.png' className='profileIcon'></img> }
-
-          </Link>
-
-        </div>
-      </>
-    );
-  }
-}
+const NavigationBar = ({ user }) => (
+  <div>
+    <Link to="/" exact="true">
+      {user && <input type="image" src="https://img.icons8.com/material/452/home--v5.png" className="homePageIcon" alt="homePageImg" /> }
+    </Link>
+    <h1 className="Harmony">harmony</h1>
+    <Link to="/profile">
+      {user && <img src="http://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Image.png" className="profileIcon" alt="profileImg" /> }
+    </Link>
+  </div>
+);
 
 export default NavigationBar;
-
